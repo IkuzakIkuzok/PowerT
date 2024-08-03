9240eaf4dff0ac70a074a3a09501f5a7b9f3ef7a
ikuzak <k.kohzuki@gmail.com>
2024-08-04 00:33:44 +0900

    Filename format

--- PowerT/Controls/Concatenator/FileNameHandler.cs
+++ PowerT/Controls/Concatenator/FileNameHandler.cs
@@ -50,6 +50,6 @@ internal static partial class FileNameHandler
         }
     } // internal static string GetFileName (string, string)
 
-    [GeneratedRegex(@"<BASENAME(\|[^|=]+=[^|=]+)*>", RegexOptions.Compiled)]
+    [GeneratedRegex(@"<BASENAME(\|[^|=]+=[^|=]*)*>", RegexOptions.Compiled)]
     private static partial Regex BasenamePattern();
 } // internal static class FileNameHandler

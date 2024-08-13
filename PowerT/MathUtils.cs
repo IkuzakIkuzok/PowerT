48221a0e59d716bde0709a6a3a0e28d45fd97d0e
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-08-13 19:18:15 +0900

    Add SafeAbs method in MathUtils with error handling

---
+++ PowerT/MathUtils.cs
@@ -0,0 +1,30 @@
+﻿
+// (c) 2024 Kazuki Kohzuki
+
+namespace PowerT;
+
+/// <summary>
+/// Provides mathematical utilities.
+/// </summary>
+internal static class MathUtils
+{
+    /// <summary>
+    /// Calculates the absolute value of the specified integer
+    /// without throwing an exception in case of overflow.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>The absolute avalue of the <paramref name="value"/>.</returns>
+    /// <remarks>If the <paramref name="value"/> is <see cref="int.MinValue"/>,
+    /// the return value is <see cref="int.MaxValue"/>, which is 1 less than the exact value.</remarks>
+    internal static int SafeAbs(this int value)
+    {
+        try
+        {
+            return Math.Abs(value);
+        }
+        catch (OverflowException)
+        {
+            return int.MaxValue;
+        }
+    } // internal static int SafeAbs (this int)
+} // internal static class MathUtils

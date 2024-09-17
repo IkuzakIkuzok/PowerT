d6a1fbd669b4d89577f7631ac2265ef7f5127b29
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-09-17 13:58:38 +0900

    Add dll project for plugin
    - IPlugin
    - ISmoother

---
+++ PowerT.Plugin/IPlugin.cs
@@ -0,0 +1,30 @@
+﻿
+// (c) 2024 Kazuki Kohzuki
+
+namespace PowerT.Plugin;
+
+/// <summary>
+/// Represents a plugin.
+/// </summary>
+public interface IPlugin
+{
+    /// <summary>
+    /// Gets the name of the plugin.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Gets the description of the plugin.
+    /// </summary>
+    public string Description { get; }
+
+    /// <summary>
+    /// Initializes the plugin.
+    /// </summary>
+    public void Initialize();
+
+    /// <summary>
+    /// Disposes the plugin.
+    /// </summary>
+    public void Dispose();
+} // public interface IPlugin
---
+++ PowerT.Plugin/ISmoother.cs
@@ -0,0 +1,35 @@
+﻿
+// (c) 2024 Kazuki Kohzuki
+
+namespace PowerT.Plugin;
+
+/// <summary>
+/// Represents a smoother.
+/// </summary>
+public interface ISmoother : IPlugin
+{
+    /// <summary>
+    /// Gets a value indicating whether the smoother has an option.
+    /// </summary>
+    bool HasOption { get; }
+
+    /// <summary>
+    /// Smoothes the data.
+    /// </summary>
+    /// <param name="data">The data to be smoothed.</param>
+    /// <returns>The smoothed data.</returns>
+    public IEnumerable<double> Smooth(IEnumerable<double> data);
+
+    /// <summary>
+    /// Sets the option.
+    /// </summary>
+    /// <param name="option">The option.</param>
+    /// <returns><see langword="true"/> if the option is set successfully; otherwise, <see langword="false"/>.</returns>
+    public bool SetOption(string option);
+
+    /// <summary>
+    /// Gets the current option.
+    /// </summary>
+    /// <returns></returns>
+    public string GetOption();
+} // public interface ISmoother

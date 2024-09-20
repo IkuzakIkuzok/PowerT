1fc5799cb9223d91f8375f891e77b6cddecfa7ad
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-09-20 12:08:27 +0900

    Add LinerWeightedMovingAverageSmoother class
    Introduce LinerWeightedMovingAverageSmoother in PowerT.Data.
    This class smooths data using a weighted moving average method.
    It inherits from MovingAverageSmootherBase and overrides:
    - Name property: returns "Liner Weighted Moving Average".
    - Description property: describes the smoothing method.
    - Smooth method: implements the weighted moving average logic.

---
+++ PowerT/Data/LinerWeightedMovingAverageSmoother.cs
@@ -0,0 +1,43 @@
+﻿
+// (c) 2024 Kazuki Kohzuki
+
+using System.Runtime.InteropServices;
+
+namespace PowerT.Data;
+
+/// <summary>
+/// Represents a smoother using the weighted moving average method.
+/// </summary>
+[Guid("E152F675-5600-46AE-82FC-83B1A76114A9")]
+internal sealed class LinerWeightedMovingAverageSmoother : MovingAverageSmootherBase
+{
+    /// <inheritdoc/>
+    override public string Name
+        => "Liner Weighted Moving Average";
+
+    /// <inheritdoc/>
+    override public string Description
+        => "Smoothes the data using the weighted moving average method.";
+
+    /// <inheritdoc/>
+    override public IEnumerable<double> Smooth(IEnumerable<double> data)
+    {
+        var source = data.ToArray();
+        var result = new double[source.Length];
+        var halfWidth = this.width >> 1;
+        for (var i = 0; i < source.Length; i++)
+        {
+            var sum = 0.0;
+            var count = 0.0;
+            for (var j = i - halfWidth; j <= i + halfWidth; j++)
+            {
+                if (j < 0 || j >= source.Length) continue;
+                var weight = 1.0 - MathUtils.SafeAbs(i - j) / halfWidth;
+                sum += source[j] * weight;
+                count += weight;
+            }
+            result[i] = sum / count;
+        }
+        return result;
+    } // override public IEnumerable<double> Smooth (IEnumerable<double>)
+} // internal sealed class LinerWeightedMovingAverageSmoother : MovingAverageSmootherBase
